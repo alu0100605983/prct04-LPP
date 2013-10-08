5bd21c8e2bf4d1259628e395541e6da19317ac15
Maria Rojas <alu0100605983@ull.edu.es>
2013-10-08 18:18:51 +0200

    Creación de Matrices

---
+++ matrices.rb
@@ -0,0 +1,10 @@
+=begin
+	Lenguajes y Paradigmas de Programación
+	Practica 4: Suma y multipliación de Matrices en Ruby
+	Autores: Guillermo Rivero y María Rojas
+=end
+
+a = [[1,2,3], [2,3,5], [3,4,2]]
+b = [[1,6,4], [5,3,4], [2,3,6]]
+
+mult = Array.new(3){Array.new(3)}
