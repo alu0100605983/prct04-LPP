e47d420cdba7599e9ab4e911706afb5e9d39d2e7
Maria Rojas <alu0100605983@ull.edu.es>
2013-10-08 18:35:49 +0200

    Maquetado todo

--- matrices.rb
+++ matrices.rb
@@ -1,33 +1,17 @@
-a = [[1,2,3],[2,3,4],[5,6,7]]
-
-b = [[2,3,4],[5,6,7],[8,9,10]]
-
-sum = [[0,0,0],[0,0,0],[0,0,0]]
-
-
-def suma(e,f)
-d=[0,0,0],[0,0,0],[0,0,0]
-
-	for i in(0..2)
-		for j in(0..2)
-			d[i][j]=e[i][j]+f[i][j]			
-		end
-	end
-return d
-end
-
-sum=suma(a,b)
-puts sum
-
-
 =begin
 	Lenguajes y Paradigmas de Programación
 	Practica 4: Suma y multipliación de Matrices en Ruby
 	Autores: Guillermo Rivero y María Rojas
 =end
 
+a = [[1,2,3],[2,3,4],[5,6,7]]
+
+b = [[2,3,4],[5,6,7],[8,9,10]]
+
+sum = [[0,0,0],[0,0,0],[0,0,0]]
 
 mult = Array.new(3){Array.new(3)}
+
 
 puts "Matriz A"
 for i in (0..2)
@@ -46,6 +30,31 @@ for i in (0..2)
 	end
 	print "\n"
 end
+
+# Suma de matrices
+def suma(e,f)
+d=[0,0,0],[0,0,0],[0,0,0]
+
+	for i in(0..2)
+		for j in(0..2)
+			d[i][j]=e[i][j]+f[i][j]			
+		end
+	end
+return d
+end
+
+sum=suma(a,b)
+puts "Suma de A y B"
+for i in (0..2)
+	for j in (0..2)
+		print sum[i][j]
+		print " "
+	end
+	print "\n"
+end
+
+
+
 
 # Multiplicación de matrices
 
