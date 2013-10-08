23181d5f878f0b607159dfafdd940cbda1c1132a
Maria Rojas <alu0100605983@ull.edu.es>
2013-10-08 18:22:00 +0200

    Métodos para mostrar las matrices

--- matrices.rb
+++ matrices.rb
@@ -8,3 +8,21 @@ a = [[1,2,3], [2,3,5], [3,4,2]]
 b = [[1,6,4], [5,3,4], [2,3,6]]
 
 mult = Array.new(3){Array.new(3)}
+
+puts "Matriz A"
+for i in (0..2)
+	for j in (0..2)
+		print a[i][j]
+		print " "
+	end
+	print "\n"
+end
+
+puts "Matriz B"
+for i in (0..2)
+	for j in (0..2)
+		print b[i][j]
+		print " "
+	end
+	print "\n"
+end
